613ba6b228faf048070a250a91114d3d530495f8
dmytrolazoriv <lazarevdmitro1@gmail.com>
2023-02-04 14:32:11 +0200

    Added Product Form(first part)

--- src/script.js
+++ src/script.js
@@ -2,4 +2,20 @@ import Alpine from 'alpinejs'
  
 window.Alpine = Alpine
  
-Alpine.start()
+Alpine.start();
+
+//  // import Swiper bundle with all modules installed
+//  import Swiper from 'swiper/bundle';
+
+//  // init Swiper:
+//  const swiper = new Swiper('.swiper', {
+//     direction: 'horizontal',
+//     loop: true,
+//     pagination: {
+//       el: '.swiper-pagination',
+//     },
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev'
+//     }
+//   });
